dd4a2e46b9fbc149efd3165296498d5a376f8826
nishkaush <kaushish.nishant@hotmail.com>
2019-04-03 00:20:47 +1100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,158 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Valiant Notes</h1>
+    <small>( Click on any note's text to edit it )</small>
+    <!-- Form for Adding new notes -->
+    <form class="add_new_note_form" @submit.prevent="addNewNote">
+      <input type="text" v-model="newNoteText">
+      <button class="add_new_note_btn">Add Note</button>
+    </form>
+
+    <div class="note_container_div" v-for="note in allNotes" :key="note.id">
+      <!-- shown normally for each Note -->
+      <div class="note" v-if="noteToEdit.id!==note.id">
+        <span @click="editNote(note.id)">{{note.text}}</span>
+        <i class="material-icons delete" @click="deleteNote(note.id)">done</i>
+      </div>
+      <!-- only shown when that Note goes into edit mode -->
+      <div class="editing_note" v-if="showEditMode && noteToEdit.id===note.id">
+        <input type="text" v-model="noteToEdit.text">
+        <i @click="saveNote(note.id)" class="material-icons save">save</i>
+        <i @click="cancelNoteEditing" class="material-icons cancel">clear</i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      noteToEdit: {},
+      showEditMode: false,
+      newNoteText: "",
+      notesArr: [
+        // { id: 2323424200, text: "First Note", status: false }
+      ]
+    };
+  },
+  methods: {
+    editNote(noteId) {
+      let editableNote = this.notesArr.find(e => e.id === noteId);
+      this.noteToEdit = { text: editableNote.text, id: editableNote.id };
+      this.showEditMode = true;
+    },
+    saveNote(noteId) {
+      let updatedObj = { ...this.noteToEdit, status: false };
+      let indexOfNoteToUpdate = this.notesArr.findIndex(e => e.id === noteId);
+      this.notesArr[indexOfNoteToUpdate] = updatedObj;
+      this.cancelNoteEditing();
+    },
+    cancelNoteEditing() {
+      this.showEditMode = false;
+      this.noteToEdit = {};
+    },
+    addNewNote() {
+      let newNoteObj = {
+        id: Date.now(),
+        text: this.newNoteText,
+        status: false
+      };
+      this.notesArr.push(newNoteObj);
+      this.newNoteText = "";
+    },
+    deleteNote(noteId) {
+      this.notesArr = this.notesArr.filter(e => e.id !== noteId);
+    }
+  },
+  computed: {
+    allNotes() {
+      return this.notesArr.sort((a, b) => b.id - a.id);
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: #313131;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+}
+
+input {
+  width: 60%;
+  height: 35px;
+  border: none;
+  background: transparent;
+  border-bottom: 1px solid #dedede;
+  color: white;
+  font-size: 1em;
+}
+input:focus {
+  outline: none;
+}
+
+.add_new_note_form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  margin: 0 auto;
+}
+
+.add_new_note_btn {
+  width: 160px;
+  height: 45px;
+  background: blue;
+  color: white;
+  border: none;
+  box-shadow: 1px 1px 1px 1px solid #323232;
+  border-radius: 2px;
+  margin: 2%;
+  text-transform: uppercase;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.note_container_div {
+  text-align: left;
+  width: 50%;
+  margin: 1% auto;
+  font-size: 0.8em;
+}
+
+.material-icons {
+  margin: auto 1%;
+  font-size: 30px;
+  cursor: pointer;
+}
+.material-icons.delete {
+  color: rgb(11, 235, 11);
+}
+
+.note,
+.editing_note {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  padding: 1% 2%;
+}
+.note {
+  border-radius: 5px;
+  box-shadow: 1px 1px 2px 0px #3f3f3f;
 }
 </style>
